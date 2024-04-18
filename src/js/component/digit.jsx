d4030cf60cf9f5515514a676886a102d271b3d98
Marcos Cabral Peralta <156668187+Marcos-93LAB@users.noreply.github.com>
2024-04-18 11:58:14 +0000

    second commit

---
+++ src/js/component/digit.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Digit= ({number}) => <div className="digit">{number}</div> 
+
+export default Digit;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import Card from "./Card";
+import React, { useEffect, useState } from "react";
+import Digit from "./digit";
 
 //create your first component
 const Home = () => {
+	//estados sin asincronos
+	const [timer, setTimer] = useState(0)
+
+	useEffect(() => {
+
+		setTimeout(() => {
+			setTimer(value => value + 1)
+		}, 1000);
+
+	}, [timer])//array de dependencias 
+
 	return (
-		<div>
-			<Card/>
-		</div>
+		<main className="text-center">
+
+			<section className="counter-holder">
+			<Digit number={<span className="fa fa-clock"></span>}/>
+			<Digit number={Math.floor(timer / 100000) % 10}/>
+			<Digit number={Math.floor(timer / 10000) % 10}/>
+			<Digit number={Math.floor(timer / 1000) % 10}/>
+			<Digit number={Math.floor(timer / 100) % 10}/>
+			<Digit number={Math.floor(timer / 10) % 10}/>
+			<Digit number={Math.floor(timer % 10)}/>
+			</section>
+		</main>
 	);
 };
 
